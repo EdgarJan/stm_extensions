a86274a110e44e9ea5bfbd7baaeb2ba5ba24acb8
Edgar Jan <EdgarJan@users.noreply.github.com>
2023-06-07 11:48:03 +0300

    replaceLithuanianCharacters

--- lib/stm_extensions.dart
+++ lib/stm_extensions.dart
@@ -1,7 +1,37 @@
 library stm_extensions;
 
 extension STMExtensions on String {
+  //capitalizes first letter, and do not change anythin else
   String onlyCapitalizeFirst() {
     return this[0].toUpperCase() + substring(1);
+  }
+
+  String replaceLithuanianCharacters(String input) {
+    final Map<String, String> replacements = {
+      'ą': 'a',
+      'č': 'c',
+      'ę': 'e',
+      'ė': 'e',
+      'į': 'i',
+      'š': 's',
+      'ų': 'u',
+      'ū': 'u',
+      'ž': 'z',
+      'Ą': 'A',
+      'Č': 'C',
+      'Ę': 'E',
+      'Ė': 'E',
+      'Į': 'I',
+      'Š': 'S',
+      'Ų': 'U',
+      'Ū': 'U',
+      'Ž': 'Z'
+    };
+    String result = input;
+    replacements.forEach((lithuanian, english) {
+      result = result.replaceAll(lithuanian, english);
+    });
+
+    return result;
   }
 }
